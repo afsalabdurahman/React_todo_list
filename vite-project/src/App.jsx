8025702c34ba2974a027f62814330c7584f3b85a
Afsal kp <afsalkp4343@gmail.com>
2025-01-13 18:44:29 +0530

    completed

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  let [todos, setTodo] = useState([]);
+  let [value, setValue] = useState("");
+
+  console.log(todos, "values");
+
+  return (
+    <>
+      <div class='container'>
+        <h1>My To-Do List</h1>
+        <div class='todo-input'>
+          <input
+            type='text'
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            id='taskInput'
+            placeholder='Add a new task...'
+          />
+          <button
+            id='addTaskBtn'
+            onClick={() => {
+              setTodo([...todos, { input: value, key: Date.now(), status: false }]);
+            }}
+          >
+            Add Task
+          </button>
+        </div>
+        {todos.map((e) => {
+          return (
+            <ul
+              id='taskList'
+              style={{
+                listStyle: "none",
+                padding: 0,
+              }}
+            >
+              <li
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  backgroundColor: "#f8f9fa",
+                  padding: "12px",
+                  borderRadius: "8px",
+                  marginBottom: "8px",
+                }}
+              >
+                {!e.status ?
+                  <input
+                    onClick={() => {
+                      setTodo(todos.map((todo) => {
+                        if (todo.input == e.input) {
+                          return { ...todo, status: true };
+                        }
+
+                        return todo
+                      }))
+                    }}
+                    id='status-box'
+                    type='checkbox'
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      cursor: "pointer",
+                    }}
+                  />
+                  : <div class="line1">
+                    <span class="line-text1">{e.input}</span>
+                  </div>}
+                <span
+                  style={{
+                    flex: 1,
+                    fontSize: "16px",
+                  }}
+                >
+                  {!e.status ? e.input : null}
+                </span>
+                {!e.status ?
+                  <button
+                    onClick={() => {
+                      setTodo(todos.filter((todo) => todo.input !== e.input));
+                    }}
+                    style={{
+                      backgroundColor: "#e74c3c",
+                      color: "white",
+                      border: "none",
+                      padding: "8px 15px",
+                      borderRadius: "6px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    delete
+                  </button>
+                  : null}
+              </li>
+
+            </ul>
+
+          );
+
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
